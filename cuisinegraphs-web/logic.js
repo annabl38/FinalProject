7f79275110745a0fb3cf7f81e9b08139e8e65571
Kim.McK <k.mckeever@alumni.unc.edu>
2019-07-15 21:42:40 -0400

    merge attempt 2

--- cuisinegraphs-web/logic.js
+++ cuisinegraphs-web/logic.js
@@ -15,8 +15,8 @@
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
     
     // create 2 data_set
-    var data1 = {a: 9, b: 20, c:30, d:8, e:12}
-    var data2 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
+    var data01 = {a: 9, b: 20, c:30, d:8, e:12}
+    var data02 = {a: 6, b: 16, c:20, d:14, e:19, f:12}
     
     // set the color scale
     var color = d3.scaleOrdinal()
